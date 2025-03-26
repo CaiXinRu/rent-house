69f992f34e527f06ea11b092bbeddef06582bb18
naomi <naomi@program.com.tw>
2025-03-26 17:44:01 +0800

    feat: [mapping] GPS & set userLocation

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,6 @@
 import { icon, LatLngLiteral } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   MapContainer,
   Marker,
@@ -9,12 +9,10 @@ import {
   ZoomControl,
 } from "react-leaflet";
 
-// type MapType = "roadmap" | "satellite" | "hybrid" | "terrain";
-
 type MapLocation = LatLngLiteral & { id: string };
 
 type MapProps = {
-  center: LatLngLiteral;
+  // center: LatLngLiteral;
   locations: MapLocation[];
 };
 
@@ -24,21 +22,11 @@ const SelectedLocation = ({ center }: { center: LatLngLiteral }) => {
   return null;
 };
 
-export const Map: React.FC<MapProps> = ({ center, locations }) => {
-  // const [mapType, setMapType] = useState<MapType>("roadmap");
+export const Map: React.FC<MapProps> = ({ locations }) => {
   const [selectedLocation, setSelectedLocation] = useState<
     MapLocation | undefined
   >();
-
-  // const getUrl = () => {
-  //   const mapTypeUrls: Record<MapType, string> = {
-  //     roadmap: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  //     satellite: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  //     hybrid: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  //     terrain: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  //   };
-  //   return mapTypeUrls[mapType];
-  // };
+  const [userLocation, setUserLocation] = useState<LatLngLiteral | null>(null);
 
   const mapMarkIcon = icon({
     iconUrl: "map-marker.png",
@@ -48,6 +36,11 @@ export const Map: React.FC<MapProps> = ({ center, locations }) => {
   const mapMarkActiveIcon = icon({
     iconUrl: "active-map-marker.png",
     iconSize: [57, 64],
+  });
+
+  const userIcon = icon({
+    iconUrl: "user-location.png",
+    iconSize: [45, 45],
   });
 
   const renderMarks = () => {
@@ -70,18 +63,41 @@ export const Map: React.FC<MapProps> = ({ center, locations }) => {
     ));
   };
 
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ lat: latitude, lng: longitude });
+        },
+        (error) => {
+          console.log("Error getting user location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  const defaultCenter: LatLngLiteral = { lat: 23.5, lng: 120 };
+  console.log(userLocation)
+
   return (
     <div className="w-full h-screen overflow-hidden">
       <MapContainer
-        center={center}
-        zoom={13}
+        key={userLocation ? "user" : "default"}
+        center={userLocation || defaultCenter}
+        zoom={userLocation ? 15 : 7}
         minZoom={5}
         zoomControl={false}
         attributionControl={false}
         style={{ width: "100%", height: "100%" }}
       >
         <TileLayer url="http://mt0.google.com/vt/lyrs=m&hl=zh-TW&x={x}&y={y}&z={z}" />
+        {/* 選中的標記，將地圖移動過去 */}
         {selectedLocation && <SelectedLocation center={selectedLocation} />}
+        {/* 使用者位置標記 */}
+        {userLocation && <Marker position={userLocation} icon={userIcon} />}
         {renderMarks()}
         <ZoomControl position="topright" />
       </MapContainer>
